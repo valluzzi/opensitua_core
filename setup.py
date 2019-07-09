626469406264dc115b3b9583fc4b3da53b30a557
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2019-07-09 13:45:31 +0200

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 PACKAGE_NAME = "opensitua_core"
 
 def get_version():
-    VERSIONFILE = os.path.join(PACKAGE_NAME, '__init__.py')
+    VERSIONFILE = os.path.join( '__init__.py')
     initfile_lines = open(VERSIONFILE, 'rt').readlines()
     VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
     for line in initfile_lines:
